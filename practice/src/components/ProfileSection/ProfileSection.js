228536c56710ea0ef7cec90acee758b73d29aacb
Dashiell BarkHuss <dashiellbarkhuss@gmail.com>
2020-11-18 14:16:59 -0600

    writing out what functions we'll need for each state

--- practice/src/components/ProfileSection/ProfileSection.js
+++ practice/src/components/ProfileSection/ProfileSection.js
@@ -1,7 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ProfileSection.css';
-import CoverImage from './CoverImage/CoverImage.js';
-import ProfileInfo from './ProfileInfo/ProfileInfo';
 import EditableProfileInfo from './EditbableProfileInfo/EditableProfileInfo.js';
 import EditableCoverImage from './EditableCoverImage/EditableCoverImage';
 import Button from '@material-ui/core/Button';
@@ -15,6 +13,28 @@ import Button from '@material-ui/core/Button';
  * @param  props.profileMessage
  */
 function ProfileSection(props) {
+  const [profilePicture, setProfilePicture] = useState(null);
+  const [coverImage, setCoverImage] = useState(null);
+  const [wishlistName, setWishlistName] = useState(null);
+  const [handle, setHandle] = useState(null);
+  const [message, setMessage] = useState(null);
+
+  // route post /alias/update
+  // profilePicture
+  // handle
+
+  const handleUpdateProfilePicture = () => {};
+  const handleCheckHandleAvailability = () => {};
+  const handleUpdateHandle = () => {};
+
+  // route post /wishlist update
+  // coverImage
+  // message
+  // wishlistName
+  const handleUpdateCoverImage = () => {};
+  const handleUpdateMessage = () => {};
+  const handleUpdateWishlistName = () => {};
+
   return (
     <div className="profile_section">
       <EditableCoverImage coverPicUrl={props.coverPicUrl}></EditableCoverImage>
